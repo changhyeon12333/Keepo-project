3a67474956ebeea566ad721dda23fea85a431316
Lee chang <changhyeon12333@gmail.com>
2022-05-25 20:35:21 +0900

    make intro

---
+++ app/src/main/java/com/example/keepo_app/IntroActivity.kt
@@ -0,0 +1,38 @@
+package com.example.keepo_app
+
+import android.app.ActionBar
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.keepo_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class introlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.intro_layout)
+
+        val actionBar: androidx.appcompat.app.ActionBar? = supportActionBar
+        actionBar!!.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }, 5000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
