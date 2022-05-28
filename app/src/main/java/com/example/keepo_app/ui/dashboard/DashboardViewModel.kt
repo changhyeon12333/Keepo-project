4067c816cf34f2610e87f646fd2ddf30ed2d227a
Lee chang <changhyeon12333@gmail.com>
2022-05-28 20:53:16 +0900

    spinepage_one

--- app/src/main/java/com/example/keepo_app/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/keepo_app/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "KeePo 자세교정"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/keepo_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/keepo_app/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "척추건강 검사를 시작합니다."
     }
     val text: LiveData<String> = _text
 }
