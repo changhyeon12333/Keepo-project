1464a9024afa45447fb1413bf06c39a5017b7c06
9incero <9incero@gmail.com>
2022-05-30 20:52:08 +0900

    disease new window

--- app/src/main/java/com/example/keepo_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/keepo_app/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.example.keepo_app.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.keepo_app.HomeActivity
+import com.example.keepo_app.MainActivity
+import com.example.keepo_app.R
 import com.example.keepo_app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -27,7 +32,17 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        val view = inflater.inflate(R.layout.fragment_home, null)
+        val loginButton = view.findViewById<Button>(R.id.button_disease)
+        loginButton.setOnClickListener(object :View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(context, HomeActivity::class.java)
+                startActivity(intent)
+                // 다른 액티비티에서 전환할 때
+                // activity?.finish()
+            }
+        })
+        return view
 
         return root
     }
